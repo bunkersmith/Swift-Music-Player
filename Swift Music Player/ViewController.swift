2bee47601a216806b402d847cfecb6f1e4bb2dd4
Carl Smith <carl@afterburnerimages.com>
2016-05-21 14:55:00 -0700

    Initial Commit

---
+++ Swift Music Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Music Player
+//
+//  Created by CarlSmith on 5/21/16.
+//  Copyright © 2016 CarlSmith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
